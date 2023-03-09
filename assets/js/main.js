6738857149e3f74eec6b3e6bb4be3b0988d5ab00
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-09 18:39:47 +0100

    ADD Ticket preview content and basic layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,23 +25,43 @@ const ticketPreviewEl = document.getElementById("ticket_preview");
 
 // GENERATE FUNCTION
 generateBtn.addEventListener('click', function() {
+  const userName = document.getElementById("name_surname");
   const km = Number(document.getElementById("km").value);
   const age = document.getElementById("age").value;
+
+  // CHECK IF EVERY INPUT IS FILLED
+  if (userName == "" || km == 0) {
+    alert("ERRORE. Tutti i campi devono essere compilati");
+    return;
+  }
+
   const costPerKm = 0.21;
   let finalPrice = km * costPerKm;
+
+  document.getElementById("table_offer_type").innerText = "Biglietto Standard";
 
   switch (age) {
     case "minorenne":
       finalPrice *= 0.8;
-      console.log(`Prezzo minorenne: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`);
+      document.getElementById("table_offer_type").innerText = "Biglietto Under 18";
       break;
     case "over65":
       finalPrice *= 0.6;
-      console.log(`Prezzo over 65: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`);
+      document.getElementById("table_offer_type").innerText = "Biglietto Over 65";
       break;
-    default:
-      console.log(`Prezzo maggiorenne: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`);
   }
+
+  document.getElementById("table_passenger_name").innerText = userName.value;
+  document.getElementById("table_kms").innerText = km;
+
+  // MAX 20 TRAIN CARRIAGE
+  document.getElementById("table_train_carriage").innerText = Math.floor(Math.random() * 20) + 1;
+
+  // 5 DIGITS INTEGER NUMBER NEVER STARTING WITH 0
+  document.getElementById("table_cp_code").innerText = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
+
+  document.getElementById("table_ticket_cost").innerText = `${finalPrice}€`;
+
 
   // SHOWS THE TICKET PREVIEW BY REMOVING BOOTSTRAP CLASS D-NONE
   ticketPreviewEl.classList.remove("d-none");
